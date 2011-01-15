770e183cfcd59ac6eb5eef0266499b1deca546b1
tom <tom@toms-MacBook-Pro.local>
2011-01-15 12:47:54 -0500

    adding los class

---
+++ los.cpp
@@ -0,0 +1,11 @@
+/*
+ *  los.cpp
+ *  gltest
+ *
+ *  Created by tom on 1/15/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "los.h"
+
---
+++ los.h
@@ -0,0 +1,20 @@
+/*
+ *  los.h
+ *  gltest
+ *
+ *  Created by tom on 1/15/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef _LOS_H_
+#define _LOS_H_
+
+class LineOfSight
+{
+public:
+private:
+};
+
+#endif // _LOS_H_
+
