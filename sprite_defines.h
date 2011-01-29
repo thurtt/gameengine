90aceb0afe4a9f21c3f2929943541db476e2bab3
Stuart Templeton <bvrn007@gmail.com>
2011-01-29 10:56:35 -0500

    added the defines required.

---
+++ sprite_defines.h
@@ -0,0 +1,20 @@
+/*
+ *  sprite_defines.h
+ *  gltest
+ *
+ *  Created by Stuart Templeton on 1/29/11.
+ *  Copyright 2011 none. All rights reserved.
+ *
+ */
+
+
+#ifndef _SPRITEDEFINES_H
+#define _SPRITEDEFINES_H
+
+#define BLOCK_VISIBILITY 1
+#define BLOCK_MOVEMENT 2
+#define ALIVE 3
+#define EXPIRATION 4
+#define EXPIRATION_TICK 5
+
+#endif // _SPRITEDEFINES_H
