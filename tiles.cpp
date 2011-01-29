64d4a092423a6386631e8cb2356ce04814f87257
Stuart Templeton <bvrn007@gmail.com>
2011-01-28 20:35:41 -0500

    made a blocking tile.

--- tiles.cpp
+++ tiles.cpp
@@ -99,7 +99,7 @@ void populateTileSet(){
 	tileset.push_back(raw_tile3);
 	
 	vector<_sprite_data*> raw_tile4 ;
-	raw_tile4.push_back( new _sprite_data(0,0,TILESIZE,TILESIZE,"clean_tiles.png", false, false));
+	raw_tile4.push_back( new _sprite_data(0,0,TILESIZE,TILESIZE,"clean_tiles.png", true, true)); //this will blockmovement.
 	raw_tile4.push_back( new _sprite_data(0,0,TILESIZE,TILESIZE,"tile_blood.png", false, false));
 	tileset.push_back(raw_tile4);
 }
